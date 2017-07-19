d77d553be5298ca405e6ff528bd3ada8a909dee6
jon <jgkerkhoven@gmail.com>
2017-07-19 14:54:51 +0200

    added modules folder for styleguide changes and normalized color schemes.

--- src/sections/header.php
+++ src/sections/header.php
@@ -10,14 +10,14 @@
   	<title>Jonathan Kerkhoven</title>
   </head>
   <body>
-    <header>
-      <nav>
-        <ul>
-          <li>home</li>
-          <li>about</li>
-          <li>experience</li>
-          <li>images</li>
-          <li>contact</li>
+    <header class="header">
+      <nav class="navigation">
+        <ul class="navigation__list">
+          <li class="navigation__item">home</li>
+          <li class="navigation__item">about</li>
+          <li class="navigation__item">experience</li>
+          <li class="navigation__item">images</li>
+          <li class="navigation__item">contact</li>
         </ul>
       </nav>
     </header>
