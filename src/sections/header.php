7b1ec5bf0b053fca139be06afdd686808178c51f
jon <jgkerkhoven@gmail.com>
2017-07-19 14:08:00 +0200

    changed the style.scss for consistency, changed css path to the right file.

--- src/sections/header.php
+++ src/sections/header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
   	<meta name="viewport" content="width=device-width, initial-scale=1">
-  	<link rel="stylesheet" type="text/css" href="main.css">
+  	<link rel="stylesheet" type="text/css" href="build/css/style.css">
   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
   	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&amp;lang=en" />
   	<link rel="icon" type="image/png" sizes="16x16" href="img/backgrounds/favicon-16x16.png">
