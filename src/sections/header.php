1525c7ac80b40bdd058964c47d60042047d3c6cc
jon <jgkerkhoven@gmail.com>
2017-07-19 15:14:53 +0200

    added colors and typo scss, changed font

--- src/sections/header.php
+++ src/sections/header.php
@@ -5,7 +5,7 @@
   	<meta name="viewport" content="width=device-width, initial-scale=1">
   	<link rel="stylesheet" type="text/css" href="build/css/style.css">
   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-  	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&amp;lang=en" />
+  	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Rubrik:300,400,600,700&amp;lang=en" />
   	<link rel="icon" type="image/png" sizes="16x16" href="img/backgrounds/favicon-16x16.png">
   	<title>Jonathan Kerkhoven</title>
   </head>
