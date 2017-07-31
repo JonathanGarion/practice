b052f7eccb34e490bfed91ef77c0c9b13b7f29c0
jon <jgkerkhoven@gmail.com>
2017-07-31 14:25:31 +0200

    reworked flexbox

--- src/sections/flex-section.php
+++ src/sections/flex-section.php
@@ -2,21 +2,15 @@
 	<header class="section__header"> 
 		<h2>flex section</h2>
 	</header>
-	<div class="grid">
-		<div class="grid__row">
-			<div class="grid__block block--1"></div>
-			<div class="grid__block block--2"></div>
-			<div class="grid__block block--3"></div>
-		</div>
-		<div  class="grid__row">
-			<div class="grid__block block--4"></div>
-			<div class="grid__block block--5"></div>
-			<div class="grid__block block--6"></div>
-		</div>
-		<div  class="grid__row">
-			<div class="grid__block block--7"></div>
-			<div class="grid__block block--8"></div>
-			<div class="grid__block block--9"></div>
-		</div>
+	<div class="projects__wrapper">
+			<div class="projects__block block--1"><img src="build/img/skills/img.png" alt="skill"></div>
+			<div class="projects__block block--2"><img src="build/img/skills/img.png" alt="skill"></div>
+			<div class="projects__block block--3"><img src="build/img/skills/img.png" alt="skill"></div>
+			<div class="projects__block block--4"><img src="build/img/skills/img.png" alt="skill"></div>
+			<div class="projects__block block--5"><img src="build/img/skills/img.png" alt="skill"></div>
+			<div class="projects__block block--6"><img src="build/img/skills/img.png" alt="skill"></div>
+			<div class="projects__block block--7"><img src="build/img/skills/img.png" alt="skill"></div>
+			<div class="projects__block block--8"><img src="build/img/skills/img.png" alt="skill"></div>
+			<div class="projects__block block--9"><img src="build/img/skills/img.png" alt="skill"></div>
 	</div>
 </section>
